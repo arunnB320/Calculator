0308d15713e7af0bacbc333e19ac43f6b507463b
arunnB320 <arunbaghel320@gmail.com>
2025-06-29 23:29:43 -0700

    This Calculator file

---
+++ script.js
@@ -0,0 +1,35 @@
+let isShown = false;
+function appendValue(val) {
+    const display = document.getElementById("display");
+    if (isShown) {
+        if ("0123456789.(".includes(val)) {
+            display.value += val;
+        } else {
+            display.value += val;
+        }
+        isShown = false;
+    } else {
+        display.value += val;
+    }
+}
+function Clear() {
+    document.getElementById("display").value = "";
+}
+
+function deletLast() {
+    const display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
+}
+
+function Equal() {
+    const display = document.getElementById("display");
+    try {
+        const result = math.evaluate(display.value);
+        display.value = result;
+        isShown = true;
+
+    } catch (error) {
+        alert("Invalid Expression")
+    }
+}
+
